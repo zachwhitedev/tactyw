f30689b7edb2791798a617e04d47ccf45abed4ae
Zach White <zechnwhite@gmail.com>
2019-12-29 23:13:39 -0500

    rough draft

--- main.js
+++ main.js
@@ -1,75 +1,95 @@
-
 let tlogo = document.getElementById('tshirt-logo');
 let bankroll = document.getElementById('bankroll');
 let storemessage = document.getElementById('store-message');
-
-tlogo.innerHTML = 'plebe';
+let donationstatus = document.getElementById('donation-status');
+let tossbutton = document.getElementById('get-dollar');
+donationstatus.innerHTML = 'Donation Status: Piglet';
 
 let clicked = 0;
-let bankAmt = 5;
+let bankAmt = 0;
 let storeClick = 0;
 let boughtSupreme = false;
 
-bankroll.innerHTML = `Bankroll: $${bankAmt}`
+bankroll.innerHTML = `${bankAmt}`;
 
-
-
-
-function makeSupreme(){
-boughtSupreme = true;
-if(bankAmt > 29){
-bankAmt -= 30;
-bankroll.innerHTML = `Bankroll: $${bankAmt}`
-storemessage.innerHTML = '';
-$("#tshirt-logo").css({"color": "red"});
-$("#torso").css({"background": "white"});
-tlogo.innerHTML = 'SUPREME';
-$('#character').css({'filter': 'blur(0px)'})
-$('#get-dollar').css({'display': 'block'})
-storeClick++;
-$('#store').css({'display': 'none'});
-
-
-
-}
-else {
-    storemessage.innerHTML = 'You dont have enough moola. Get back to clicking. (click \'Store\' again)';
-    tlogo.innerHTML = 'plebe';
-    $("#tshirt-logo").css({"color": "white"});
-
-}
-}
-
-
-function openStore(){
-    storeClick++
-    if(storeClick%2!=0){
-    if(tlogo.innerHTML == 'SUPREME'){
-        $('#supremey').removeAttr('onclick');
-        $('#supremey').css({'text-decoration': 'line-through'});
-
+function openStore() {
+  storeClick++;
+  if (storeClick % 2 != 0) {
+    if (tlogo.innerHTML == 'SUPREME') {
+      $('#supremey').removeAttr('onclick');
+      $('#supremey').css({ 'text-decoration': 'line-through' });
     }
-    $('#store').css({'display': 'block'})
-    $('#character').css({'filter': 'blur(5px)'})
-    $('#get-dollar').css({'display': 'none'})
+    $('#store').css({ display: 'block' });
+    $('#character').css({ filter: 'blur(5px)' });
+    $('#get-dollar').css({ display: 'none' });
     storemessage.innerHTML = '';
-    if(boughtSupreme){
-        storemessage.innerHTML = 'u bought the only fuckin thing in the store so... this is awkward! lol!';
+    if (boughtSupreme) {
+      storemessage.innerHTML =
+        'u bought the only fuckin thing in the store so... this is awkward! lol!';
     }
-
-
-
-
-    }
-    else{
-        $('#store').css({'display': 'none'})
-        $('#character').css({'filter': 'blur(0px)'})
-        $('#get-dollar').css({'display': 'block'})
-    }
+  } else {
+    $('#store').css({ display: 'none' });
+    $('#character').css({ filter: 'blur(0px)' });
+    $('#get-dollar').css({ display: 'block' });
+  }
 }
 
-function getDollar(){
-    bankAmt++;
-    bankroll.innerHTML = `Bankroll: $${bankAmt}`
+function getDollar() {
+  var audio = document.getElementById('audio');
+  audio.play();
+  bankAmt++;
+  bankroll.innerHTML = `${bankAmt}`;
+  if (bankAmt % 10 == 0) {
+    var hmm = document.getElementById('hmm');
+    hmm.play();
+  }
+  if (bankAmt == 20) {
+    $('#propic').attr('src', './jaskier.png');
+    donationstatus.innerHTML = 'Donation Status: Humble Bard';
+    var song1 = document.getElementById('song1');
+    song1.play();
+  }
+  if (bankAmt % 50 == 0) {
+    $('#propic').attr('src', './jaskier.png');
+    donationstatus.innerHTML = 'Donation Status: Humble Bard';
+    var song1 = document.getElementById('song1');
+    song1.play();
+  }
+  if (bankAmt % 100 == 0) {
+    $('#propic').attr('src', './jaskier.png');
+    donationstatus.innerHTML = 'Donation Status: Humble Bard';
+    var song1 = document.getElementById('song1');
+    song1.play();
+  }
+  if (bankAmt == 30) {
+    $('#propic').hide();
+    $('#geralt-gif').hide();
+    $('#donation-status').hide();
+    $('#bankroll').hide();
+    $('#coinpurse').hide();
+    $('#get-dollar').hide();
+    $('#roach').show();
+    $('#loyalty').show();
+    $('#startoverbtndiv').show();
 
+    donationstatus.innerHTML = 'Donation Status: Humble Bard';
+    var song1 = document.getElementById('song1');
+    song1.play();
+  }
+}
+
+function startover() {
+  $('#propic').show();
+  $('#geralt-gif').show();
+  $('#donation-status').show();
+  $('#bankroll').show();
+  $('#coinpurse').show();
+  $('#get-dollar').show();
+  $('#roach').hide();
+  $('#loyalty').hide();
+  $('#startoverbtndiv').hide();
+  bankAmt = 0;
+  bankroll.innerHTML = `${bankAmt}`;
+  $('#propic').attr('src', './yennefer.png');
+    donationstatus.innerHTML = 'Donation Status: Piglet';
 }
